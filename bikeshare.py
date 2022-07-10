3e9ac7b8e07e00e0cdd6650f4b47bc547adf0b87
Shikha Shandilya <viputiwary@gmail.com>
2022-07-10 15:38:39 +0530

    Enhanced user query message

--- bikeshare.py
+++ bikeshare.py
@@ -183,7 +183,7 @@ def main():
         user_stats(df,city)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart your search? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
